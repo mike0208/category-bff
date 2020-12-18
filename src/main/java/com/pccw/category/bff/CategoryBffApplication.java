db53914d3059eac365ce278716261428b0340bcc
mayank <mayank.gwal@ltts.com>
2020-12-18 13:58:33 +0530

    first commit

---
+++ src/main/java/com/pccw/category/bff/CategoryBffApplication.java
@@ -0,0 +1,13 @@
+package com.pccw.category.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pccw/category/bff/CategoryBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pccw.category.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
